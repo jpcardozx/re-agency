f801150b4f69b45a215c77a6b640295735df93bd
jpcardozx <jpcardozoo0106@gmail.com>
2024-09-17 21:48:00 -0300

    import fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
 import '/src/styles/Footer.css';
 
 const Footer: React.FC = () => {
@@ -10,11 +9,11 @@ const Footer: React.FC = () => {
       <img src="https://github.com/jpcardozx/react-portfolio-app/blob/main/src/assets/white-logo.png?raw=true" alt="Your Company Logo" />
     </div>
     <div className="footer-text">
-      <h2 className="footer-title">Your Company Name</h2>
-      <p className="footer-description">A brief description or tagline about your company or services.</p>
+      <h2 className="footer-title">Real Estate Agency | React + TS</h2>
+      <p className="footer-description">Portfolio Project. This is a base template for Real Estate Agency Web App.</p>
     </div>
     <div className="footer-cta">
-      <button className="get-in-touch-btn">Get in Touch</button>
+      <button className="get-in-touch-btn" href="https://jpcardozx.github.io/social-links-profile/">Get in Touch</button>
     </div>
   </div>
 </footer>
