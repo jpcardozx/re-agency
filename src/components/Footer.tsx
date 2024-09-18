cd27e451ed6788c25e3ad59fd8a271b9bd1d7e42
jpcardozx <jpcardozoo0106@gmail.com>
2024-09-17 21:49:35 -0300

    footer btn - social media

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,20 +3,27 @@ import '/src/styles/Footer.css';
 
 const Footer: React.FC = () => {
   return (
-<footer className="footer">
-  <div className="footer-content">
-    <div className="footer-logo">
-      <img src="https://github.com/jpcardozx/react-portfolio-app/blob/main/src/assets/white-logo.png?raw=true" alt="Your Company Logo" />
-    </div>
-    <div className="footer-text">
-      <h2 className="footer-title">Real Estate Agency | React + TS</h2>
-      <p className="footer-description">Portfolio Project. This is a base template for Real Estate Agency Web App.</p>
-    </div>
-    <div className="footer-cta">
-      <button className="get-in-touch-btn" href="https://jpcardozx.github.io/social-links-profile/">Get in Touch</button>
-    </div>
-  </div>
-</footer>
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-logo">
+          <img 
+            src="https://github.com/jpcardozx/react-portfolio-app/blob/main/src/assets/white-logo.png?raw=true" 
+            alt="Your Company Logo" 
+          />
+        </div>
+        <div className="footer-text">
+          <h2 className="footer-title">Real Estate Agency | React + TS</h2>
+          <p className="footer-description">
+            Portfolio Project. This is a base template for Real Estate Agency Web App.
+          </p>
+        </div>
+        <div className="footer-cta">
+          <a href="https://jpcardozx.github.io/social-links-profile/" target="_blank" rel="noopener noreferrer">
+            <button className="get-in-touch-btn">Get in Touch</button>
+          </a>
+        </div>
+      </div>
+    </footer>
   );
 }
 
